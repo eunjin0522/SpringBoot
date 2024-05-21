f6d9ac47a7678e415d228eebdce3ce16d29831aa
JJOGGAN <withmelona1@gmail.com>
2024-05-21 10:53:38 +0900

    thymeleaf 속성 학습
    fragment
    recpace
    insert
    탭 이름 바꾸기

--- demo5/src/main/java/kh/mclass/demo5/DemoController.java
+++ demo5/src/main/java/kh/mclass/demo5/DemoController.java
@@ -16,11 +16,8 @@ import kh.mclass.demo5.domain.BoardEntity;
 @Controller
 public class DemoController {
 	@GetMapping("home")
-	public String home() {
-		return "home"; //home은 tempates에 css는 static에 넣어준
+	public void commonHeader() {
 	}
-	
-	
 	@GetMapping("board/list")  //templates 파일 아래 생성된 파일명을 쓰고 주소 적기...?
 	public void boardlist(Model model, HttpSession session) {
 		//DB
