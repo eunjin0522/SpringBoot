f85a55097113cf7254372e9d7a5f5f4e881bc3a1
JJOGGAN <withmelona1@gmail.com>
2024-05-21 10:10:52 +0900

    th:each문 활용
    th:object안에 text

--- demo5/src/main/java/kh/mclass/demo5/DemoController.java
+++ demo5/src/main/java/kh/mclass/demo5/DemoController.java
@@ -1,11 +1,15 @@
 package kh.mclass.demo5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpSession;
 import kh.mclass.demo5.domain.BoardEntity;
 
 
@@ -17,17 +21,22 @@ public class DemoController {
 	}
 	
 	
-//	@GetMapping("board/list")  //templates 파일 아래 생성된 파일명을 쓰고 주소 적기...?
-//	public void boardlist(Model model, HttpSession session) {
-//		BoardEntity dto = new BoardEntity("aaa","1212","121212");
-//		model.addAttribute("board", dto);
-//		BoardEntity sessiondto = new BoardEntity("00","00TITLE","00CONTENT");
-//		session.setAttribute("demosession", sessiondto);
-//	}
+	@GetMapping("board/list")  //templates 파일 아래 생성된 파일명을 쓰고 주소 적기...?
+	public void boardlist(Model model, HttpSession session) {
+		//DB
+		List<BoardEntity> boardlist=new ArrayList<>();
+		for(int i = 0 ; i <10 ; i++) {
+			BoardEntity dto = new BoardEntity(i+"",i+"TITLE",i+"content");
+			boardlist.add(dto);
+		}
+		
+		model.addAttribute("boardlist", boardlist);
+	}
+	
 	@GetMapping("board/{boardId}")  //templates 파일 아래 생성된 파일명을 쓰고 주소 적기...?
-	public String boardlist(Model model, @PathVariable String boardId) {
+	public String boardread(Model model, @PathVariable String boardId) {
 		BoardEntity dto = new BoardEntity(boardId,boardId+"title!!!",boardId+"content!!!!!!");
-		model.addAttribute("board", dto);
+		model.addAttribute("boardread", dto);
 		return "board/read";
 	}
 	
--- demo5/src/main/java/kh/mclass/demo5/domain/BoardEntity.java
+++ demo5/src/main/java/kh/mclass/demo5/domain/BoardEntity.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor //전체생성자 , 기본생성자 못 씀
 @Component 
+
 public class BoardEntity {
 	//@NonNull //null인 상태에선 절대 안 만들어짐
 	private String boardId;
